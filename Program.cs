74ac1453b8959dbabc85addcdce6389bfd49c40a
Fiyaz007 <fiyaznazeer007@gmail.com>
2024-10-07 13:55:26 +0530

    Update 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Lab Assignment 2
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,35 @@ namespace LabAssignment2
     {
         static void Main(string[] args)
         {
-            
+            //--------------------------------------------------------
+            //1
+            //Console.WriteLine("Enter a number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"The multiplication table of {number}: ");
+            ////Printing multiplication table of a number
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"{number} * {i} = {number * i}");
+            //}
+
+            //--------------------------------------------------------
+            //2
+            //Console.WriteLine("Enter the no of rows: ");
+            //int rows = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //--------------------------------------------------------
+            //3
+
+
+            Console.ReadLine();
         }
     }
 }
